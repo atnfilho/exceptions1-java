3e724ce128a8a754d746ace96de6485bc2daf275
atnfilho <atnfilho@yahoo.com.br>
2019-11-12 22:16:31 -0300

    Good solution

--- bookingRoom/src/application/Program.java
+++ bookingRoom/src/application/Program.java
@@ -5,12 +5,12 @@
  */
 package application;
 
-import static java.lang.System.out;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 import model.entities.Reservation;
+import model.exceptions.DomainException;
 
 /**
  *
@@ -21,39 +21,39 @@ public class Program {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         
-        System.out.print("Room number:");
-        int room = sc.nextInt();
-        sc.nextLine();
-        System.out.print("Check-in date (dd/MM/yyyy):");
-        Date checkin = sdf.parse(sc.nextLine());
-        System.out.print("Check-out date (dd/MM/yyyy):");
-        Date checkout = sdf.parse(sc.nextLine());
-        Reservation res = new Reservation(room, checkin, checkout);
-        
-        System.out.println(res);
-        
-        System.out.println("");
-        System.out.println("Enter data to update the reservation:");
-        System.out.print("Check-in date: ");
-        checkin = sdf.parse(sc.nextLine());
-        System.out.print("Check-out date: ");
-        checkout = sdf.parse(sc.nextLine());
-               
-        
-        
-        String error = res.updateDates(checkin, checkout);
-        if(error != null) {
-            System.out.println("Error in reservation: " + error);
-        } else {
-            System.out.println(res);
-        }
-        
+        try {
+            
+            System.out.print("Room number:");
+            int room = sc.nextInt();
+            sc.nextLine();
+            System.out.print("Check-in date (dd/MM/yyyy):");
+            Date checkin = sdf.parse(sc.nextLine());
+            System.out.print("Check-out date (dd/MM/yyyy):");
+            Date checkout = sdf.parse(sc.nextLine());
+            Reservation res = new Reservation(room, checkin, checkout);
 
+            System.out.println(res);
+
+            System.out.println("");
+            System.out.println("Enter data to update the reservation:");
+            System.out.print("Check-in date: ");
+            checkin = sdf.parse(sc.nextLine());
+            System.out.print("Check-out date: ");
+            checkout = sdf.parse(sc.nextLine());
+
+            res.updateDates(checkin, checkout);
+        } catch(ParseException e) {
+            System.out.println("Invalid date format");
+        } catch(DomainException e) {
+            System.out.println("Error in reservation: " + e.getMessage());
+        } catch(RuntimeException e) {
+            System.out.println("Unexpected error");
+        }
         
     }
     
---
+++ bookingRoom/src/model/exceptions/DomainException.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.exceptions;
+
+/**
+ *
+ * @author Antonio
+ */
+public class DomainException extends Exception {
+
+    public DomainException(String message) {
+        super(message);
+    }
+    
+    
+    
+}
